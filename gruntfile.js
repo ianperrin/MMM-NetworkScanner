aa1ef5f8d1b0c2197af8f3b455084a51c0e5c279
Ian <ian.perrin@ntlworld.com>
2019-03-02 10:15:15 +0000

    Use .jshintrc options

--- gruntfile.js
+++ gruntfile.js
@@ -1,15 +1,21 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-    nodeunit: {
-      all: ['test/**/*.test.js']
-    },
-    jshint: {
-      all: ['*.js']
-    }
-  });
+    grunt.initConfig({
+        nodeunit: {
+            all: ['test/**/*.test.js']
+        },
+        jshint: {
+            options: {
+                jshintrc: ".jshintrc"
+            },
+            all: [
+                "*.js",
+                "!(node_modules)/*.js"
+            ]
+        },
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
-  grunt.registerTask('test', ['jshint', 'nodeunit']);
+    grunt.registerTask('test', ['jshint', 'nodeunit']);
 };
